350955b8ca849bb37c85bd6915c46edafbf475c1
aymen missaoui <aymenmissaoui56@gmail.com>
2022-11-01 12:22:44 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeadComponent } from './head/head.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule
